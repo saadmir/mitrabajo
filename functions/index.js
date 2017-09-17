fe1ea7dca6845ef972a39db4a031fbcdffbafccd
Saad Mir <saadmir@gmail.com>
2017-09-17 00:23:26 -0700

    with firebase

--- functions/index.js
+++ functions/index.js
@@ -5,26 +5,96 @@ const nexmo = new Nexmo({
     apiSecret: '7af7b87cb1b48ee0'
 });
 
-module.exports = function (context, req) {
-  context.log('JavaScript HTTP trigger function processed a request.');
-  context.log('> > > [index.js:10]', req.query.name);
-  context.log('> > > [index.js:11]', (req.body || {}).name || '');
+const admin = require("firebase-admin");
 
-  if (req.query.name || (req.body && req.body.name)) {
-    const name = req.query.name || req.body.name;
-    nexmo.message.sendSms('12016728774', '14153356477', `wassup ${name}`, function() {
-      context.log('> > > [index.js:15] sendSms', JSON.stringify(arguments, false, 4));
-      context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello Hello " + name
-      };
-      context.done();
-    });
-  } else {
-    context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
+const serviceAccount = require("./firebase.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://mitrabajo-us.firebaseio.com"
+});
+
+module.exports.makeAvailable = function (context, req) {
+  context.log('> > > [index.js:10]', req.query);
+
+  const data = req.query || {};
+  console.log('> > > [index.js:11]', `${data.msisdn}/available`);
+  admin.database().ref(`/availables_log`).push(data).then(
+    (snapshot) => {
+      context.log('> > > [firebase]');
+      _makeAvailable(data.msisdn, data.location || {});
+      nexmo.message.sendSms('12016728774', '14153356477', `wassup ${data.msisdn}`, function() {
+        context.res = {
+          // status: 200, /* Defaults to 200 */
+          body: "Hello Hello " + name
         };
-    context.done();
-  }
+        context.done();
+      // context.res.redirect(303, snapshot.ref);
+      });
+    }
+  );
 };
+
+function _makeAvailable(phone, location) {
+  admin.database().ref(`/availables/${phone}`).set({
+    isAvailable: true,
+    timestamp: Date.now(),
+    location: location || {}
+  });
+}
+
+function _makeUnavailable(phone) {
+  admin.database().ref(`/availables/${phone}`).remove();
+}
+
+// exports.makeAvailable = functions.https.onRequest((req, res) => {
+//   const data = req.query || {};
+//   console.log('> > > [index.js:11]', `${req.msisdn}/available`);
+//   admin.database().ref(`/availables_log`).push(data).then(
+//     (snapshot) => {
+//       _makeAvailable(data.msisdn, data.location || {});
+//       res.redirect(303, snapshot.ref);
+//     }
+//   );
+// });
+
+// exports.makeUnAvailable = functions.https.onRequest((req, res) => {
+//   const data = req.query || {};
+//   console.log('> > > [makeUnavailable]', `${req.msisdn}`);
+//   admin.database().ref(`/unavailables_log`).push(data).then(
+//     (snapshot) => {
+//       res.redirect(303, snapshot.ref);
+//       _makeUnavailable(req.msisdn);
+//     }
+//   );
+// });
+
+// // get list of available users
+// exports.getAvailables = functions.https.onRequest((req, res) => {
+//   // Grab the text parameter.
+//   const location = req.query.location || {};
+
+//   console.log('> > > [getAvailables]', req.query);
+// });
+
+// // msg from user to user
+// exports.sendMessage = functions.https.onRequest((req, res) => {
+//   // Grab the text parameter.
+//   const available = req.query;
+
+//   console.log('> > > [index.js:11]', `${req.msisdn}/available`);
+//   admin.database().ref(`/available/${req.query.msisdn}`).push(available).then(snapshot => {
+//     res.redirect(303, snapshot.ref);
+//   });
+// });
+
+// // rate a user
+// exports.addRating = functions.https.onRequest((req, res) => {
+//   // Grab the text parameter.
+//   const available = req.query;
+
+//   console.log('> > > [index.js:11]', `${req.msisdn}/available`);
+//   admin.database().ref(`/available/${req.query.msisdn}`).push(available).then(snapshot => {
+//     res.redirect(303, snapshot.ref);
+//   });
+// });
