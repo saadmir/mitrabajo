5ee113599d9576c5d0a9c5a1f22ffb411e507a15
Saad Mir <saadmir@gmail.com>
2017-09-16 23:43:10 -0700

    debug

--- functions/index.js
+++ functions/index.js
@@ -8,7 +8,7 @@ const nexmo = new Nexmo({
 module.exports = function (context, req) {
   context.log('JavaScript HTTP trigger function processed a request.');
   console.log('> > > [index.js:10]', req.query.name);
-  console.log('> > > [index.js:11]', req.body.name);
+  console.log('> > > [index.js:11]', (req.body || {}).name || '');
 
   if (req.query.name || (req.body && req.body.name)) {
     const name = req.query.name || req.body.name;
